fecc53a5a1ed78c46a2c3716672d7470524e772f
Barkova-Liubov <55841178+Barkova-Liubov@users.noreply.github.com>
2023-10-09 01:15:02 +0300

    Update experiment_1_regressions.R

--- experiment_1_regressions.R
+++ experiment_1_regressions.R
@@ -12,10 +12,10 @@ fillers = read_csv('experiment_1_fillers.csv')
 
 fillers %>%
   filter(Filler_type == 'coordination_island') -> coordination_island
-scope_coordination = lmer(Answer ~ Scope + (1|Protocol), data = fillers)
+scope_coordination = lmer(Answer ~ Scope + (1|Protocol), data = coordination_island)
 summary(scope_coordination)
 
 fillers %>%
   filter(Filler_type == 'adjuct_island') -> adjuct_island
-scope_adjuct = lmer(Answer ~ Scope + (1|Protocol), data = fillers)
+scope_adjuct = lmer(Answer ~ Scope + (1|Protocol), data = adjuct_island)
 summary(scope_adjuct)
